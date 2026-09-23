c122afb95264691c2be674d45101be49f077f45a
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 15:49:26 -0400

    xuyisen__diff__id_568

--- src/crewai/utilities/events/__init__.py
+++ src/crewai/utilities/events/__init__.py
@@ -1,47 +1,31 @@
-from .crew_events import (
-    CrewKickoffStartedEvent,
-    CrewKickoffCompletedEvent,
-    CrewKickoffFailedEvent,
-    CrewTrainStartedEvent,
-    CrewTrainCompletedEvent,
-    CrewTrainFailedEvent,
-    CrewTestStartedEvent,
-    CrewTestCompletedEvent,
-    CrewTestFailedEvent,
-)
-from .llm_guardrail_events import (
-    LLMGuardrailCompletedEvent,
-    LLMGuardrailStartedEvent,
-)
 from .agent_events import (
-    AgentExecutionStartedEvent,
     AgentExecutionCompletedEvent,
     AgentExecutionErrorEvent,
+    AgentExecutionStartedEvent,
 )
-from .task_events import (
-    TaskStartedEvent,
-    TaskCompletedEvent,
-    TaskFailedEvent,
-    TaskEvaluationEvent,
-)
-from .flow_events import (
-    FlowCreatedEvent,
-    FlowStartedEvent,
-    FlowFinishedEvent,
-    FlowPlotEvent,
-    MethodExecutionStartedEvent,
-    MethodExecutionFinishedEvent,
-    MethodExecutionFailedEvent,
+from .crew_events import (
+    CrewKickoffCompletedEvent,
+    CrewKickoffFailedEvent,
+    CrewKickoffStartedEvent,
+    CrewTestCompletedEvent,
+    CrewTestFailedEvent,
+    CrewTestStartedEvent,
+    CrewTrainCompletedEvent,
+    CrewTrainFailedEvent,
+    CrewTrainStartedEvent,
 )
 from .crewai_event_bus import CrewAIEventsBus, crewai_event_bus
-from .tool_usage_events import (
-    ToolUsageFinishedEvent,
-    ToolUsageErrorEvent,
-    ToolUsageStartedEvent,
-    ToolExecutionErrorEvent,
-    ToolSelectionErrorEvent,
-    ToolUsageEvent,
-    ToolValidateInputErrorEvent,
+
+# events
+from .event_listener import EventListener
+from .flow_events import (
+    FlowCreatedEvent,
+    FlowFinishedEvent,
+    FlowPlotEvent,
+    FlowStartedEvent,
+    MethodExecutionFailedEvent,
+    MethodExecutionFinishedEvent,
+    MethodExecutionStartedEvent,
 )
 from .llm_events import (
     LLMCallCompletedEvent,
@@ -50,7 +34,67 @@ from .llm_events import (
     LLMCallType,
     LLMStreamChunkEvent,
 )
-
-# events
-from .event_listener import EventListener
+from .llm_guardrail_events import (
+    LLMGuardrailCompletedEvent,
+    LLMGuardrailStartedEvent,
+)
+from .task_events import (
+    TaskCompletedEvent,
+    TaskEvaluationEvent,
+    TaskFailedEvent,
+    TaskStartedEvent,
+)
 from .third_party.agentops_listener import agentops_listener
+from .tool_usage_events import (
+    ToolExecutionErrorEvent,
+    ToolSelectionErrorEvent,
+    ToolUsageErrorEvent,
+    ToolUsageEvent,
+    ToolUsageFinishedEvent,
+    ToolUsageStartedEvent,
+    ToolValidateInputErrorEvent,
+)
+
+__all__ = [
+    "AgentExecutionCompletedEvent",
+    "AgentExecutionErrorEvent",
+    "AgentExecutionStartedEvent",
+    "CrewAIEventsBus",
+    "CrewKickoffCompletedEvent",
+    "CrewKickoffFailedEvent",
+    "CrewKickoffStartedEvent",
+    "CrewTestCompletedEvent",
+    "CrewTestFailedEvent",
+    "CrewTestStartedEvent",
+    "CrewTrainCompletedEvent",
+    "CrewTrainFailedEvent",
+    "CrewTrainStartedEvent",
+    "EventListener",
+    "FlowCreatedEvent",
+    "FlowFinishedEvent",
+    "FlowPlotEvent",
+    "FlowStartedEvent",
+    "LLMCallCompletedEvent",
+    "LLMCallFailedEvent",
+    "LLMCallStartedEvent",
+    "LLMCallType",
+    "LLMGuardrailCompletedEvent",
+    "LLMGuardrailStartedEvent",
+    "LLMStreamChunkEvent",
+    "MethodExecutionFailedEvent",
+    "MethodExecutionFinishedEvent",
+    "MethodExecutionStartedEvent",
+    "TaskCompletedEvent",
+    "TaskEvaluationEvent",
+    "TaskFailedEvent",
+    "TaskStartedEvent",
+    "ToolExecutionErrorEvent",
+    "ToolSelectionErrorEvent",
+    "ToolUsageErrorEvent",
+    "ToolUsageEvent",
+    "ToolUsageFinishedEvent",
+    "ToolUsageStartedEvent",
+    "ToolValidateInputErrorEvent",
+    "agentops_listener",
+    "crewai_event_bus",
+]
--- src/crewai/utilities/events/third_party/__init__.py
+++ src/crewai/utilities/events/third_party/__init__.py
@@ -1,1 +1,3 @@
 from .agentops_listener import agentops_listener
+
+__all__ = ["agentops_listener"]
